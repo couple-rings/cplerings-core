bc43c514b77a39754f3e1cf7e34f8da23537c280
Vincent Dao <vincentdaomain@gmail.com>
2024-08-31 16:14:22 +0700

    Update

---
+++ src/main/java/com/cplerings/rest/service/CpleringsRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cplerings.rest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpleringsRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpleringsRestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cplerings/rest/service/CpleringsRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cplerings.rest.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpleringsRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
