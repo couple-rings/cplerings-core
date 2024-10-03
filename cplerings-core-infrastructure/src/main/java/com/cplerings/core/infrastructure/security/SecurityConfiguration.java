df392c48158276974a1776b56b8c1168dc5db7c2
Vincent Dao <vincentdaomain@gmail.com>
2024-10-04 02:20:38 +0700

    Allows trailing slashes

--- cplerings-core-infrastructure/src/main/java/com/cplerings/core/infrastructure/security/SecurityConfiguration.java
+++ cplerings-core-infrastructure/src/main/java/com/cplerings/core/infrastructure/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.cplerings.core.infrastructure.security;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -26,6 +27,9 @@ public class SecurityConfiguration {
     private final JWTAuthenticationFilter jwtAuthenticationFilter;
     private final AccessDeniedHandler accessDeniedHandler;
 
+    @Value("${cplerings.api.path}")
+    private String apiPath;
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
@@ -36,6 +40,12 @@ public class SecurityConfiguration {
                 .exceptionHandling(config -> config.accessDeniedHandler(accessDeniedHandler))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(config -> config.requestMatchers(resolvePath("/docs/**"))
+                        .permitAll())
                 .build();
+    }
+
+    private String resolvePath(String path) {
+        return apiPath + path;
     }
 }
