ab4c3b31bb5cf68709a0bc0328bbd081815d64d5
CaoLinh1001 <caolinh1012003@gmail.com>
2024-10-05 12:00:55 +0700

    Set mail to null after using in test

--- cplerings-core-infrastructure/src/test/java/com/cplerings/core/test/integration/email/EmailServiceIT.java
+++ cplerings-core-infrastructure/src/test/java/com/cplerings/core/test/integration/email/EmailServiceIT.java
@@ -15,7 +15,7 @@ import com.icegreen.greenmail.util.ServerSetup;
 
 import jakarta.mail.internet.MimeMessage;
 
-class EmailServiceit extends AbstractIT {
+class EmailServiceIT extends AbstractIT {
 
     @Autowired
     private JavaMailSender javaMailSender;
@@ -45,7 +45,7 @@ class EmailServiceit extends AbstractIT {
     @AfterEach
     public void tearDown() {
         // Stop GreenMail server after each test
-        greenMail.stop();
+        greenMail = null;
     }
 
     @Test
