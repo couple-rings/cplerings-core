596ff4855284cf803feaa3d50087a28ba5405899
CaoLinh1001 <caolinh1012003@gmail.com>
2024-10-31 10:22:38 +0700

    Fix comments

--- cplerings-core-api/src/main/java/com/cplerings/core/api/design/data/DesignVersion.java
+++ cplerings-core-api/src/main/java/com/cplerings/core/api/design/data/DesignVersion.java
@@ -2,5 +2,8 @@ package com.cplerings.core.api.design.data;
 
 import com.cplerings.core.application.shared.entity.design.ADesignVersion;
 
+import lombok.Builder;
+
+@Builder
 public record DesignVersion(ADesignVersion designVersion) {
 }
--- cplerings-core-application/src/main/java/com/cplerings/core/application/design/input/CreateDesignVersionInput.java
+++ cplerings-core-application/src/main/java/com/cplerings/core/application/design/input/CreateDesignVersionInput.java
@@ -1,4 +1,7 @@
 package com.cplerings.core.application.design.input;
 
+import lombok.Builder;
+
+@Builder
 public record CreateDesignVersionInput(long designId, int versionNumber, String previewImage, String designFile) {
 }
