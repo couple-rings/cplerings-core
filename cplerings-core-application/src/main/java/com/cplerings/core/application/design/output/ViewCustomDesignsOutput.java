fe7c3f440e22c8d3b87bc0f6c6207e5c15145cf3
CaoLinh1001 <caolinh1012003@gmail.com>
2024-11-11 23:33:24 +0700

    Add IT for view custom designs

--- cplerings-core-application/src/main/java/com/cplerings/core/application/design/output/ViewCustomDesignsOutput.java
+++ cplerings-core-application/src/main/java/com/cplerings/core/application/design/output/ViewCustomDesignsOutput.java
@@ -1,14 +1,21 @@
 package com.cplerings.core.application.design.output;
 
+import com.cplerings.core.application.crafting.output.ViewCraftingRequestsOutput;
 import com.cplerings.core.application.shared.entity.design.ACustomDesign;
 import com.cplerings.core.application.shared.pagination.AbstractPaginatedOutput;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ViewCustomDesignsOutput extends AbstractPaginatedOutput<ACustomDesign> {
+
+    public static Builder builder() {
+        return new Builder();
+    }
 
     public static final class Builder extends AbstractPaginatedOutputBuilder<Builder, ViewCustomDesignsOutput, ACustomDesign> {
 
---
+++ cplerings-core-infrastructure/src/test/java/com/cplerings/core/test/integration/design/ViewCustomDesignsUseCaseIT.java
@@ -0,0 +1,69 @@
+package com.cplerings.core.test.integration.design;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.cplerings.core.api.crafting.request.ViewCraftingRequestsRequest;
+import com.cplerings.core.api.design.data.CustomDesigns;
+import com.cplerings.core.api.design.response.ViewCustomDesignsResponse;
+import com.cplerings.core.api.shared.AbstractResponse;
+import com.cplerings.core.common.api.APIConstant;
+import com.cplerings.core.domain.design.CustomDesign;
+import com.cplerings.core.test.shared.AbstractIT;
+import com.cplerings.core.test.shared.account.AccountTestConstant;
+import com.cplerings.core.test.shared.datasource.TestDataSource;
+import com.cplerings.core.test.shared.design.CustomDesignTestHelper;
+import com.cplerings.core.test.shared.helper.JWTTestHelper;
+
+public class ViewCustomDesignsUseCaseIT extends AbstractIT {
+
+    @Autowired
+    private JWTTestHelper jwtTestHelper;
+
+    @Autowired
+    private CustomDesignTestHelper customDesignTestHelper;
+
+    @Autowired
+    private TestDataSource testDataSource;
+
+    @Test
+    void givenAnyone_whenViewCustomDesigns() {
+        CustomDesign customDesign = customDesignTestHelper.createCustomDesign();
+        testDataSource.save(customDesign);
+        String token = jwtTestHelper.generateToken(AccountTestConstant.CUSTOMER_EMAIL);
+        ViewCraftingRequestsRequest request = ViewCraftingRequestsRequest.builder()
+                .page(0)
+                .pageSize(1)
+                .build();
+        final WebTestClient.ResponseSpec response = requestBuilder()
+                .path(APIConstant.VIEW_CUSTOM_DESIGNS_PATH)
+                .method(AbstractIT.RequestBuilder.Method.GET)
+                .authorizationHeader(token)
+                .query(request)
+                .send();
+        thenResponseIsOk(response);
+        thenResponseReturnCustomDesigns(response);
+
+    }
+
+    private void thenResponseReturnCustomDesigns(WebTestClient.ResponseSpec response) {
+        final ViewCustomDesignsResponse responseBody = response.expectBody(ViewCustomDesignsResponse.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertThat(responseBody)
+                .isNotNull();
+        assertThat(responseBody.getType())
+                .isEqualTo(AbstractResponse.Type.PAGINATED_DATA);
+
+        final CustomDesigns customDesigns = responseBody.getData();
+        assertThat(customDesigns).isNotNull();
+        assertThat(customDesigns.getPage()).isZero();
+        assertThat(customDesigns.getPageSize()).isEqualTo(1);
+        assertThat(customDesigns.getItems()).hasSize(1);
+        assertThat(customDesigns.getTotalPages()).isEqualTo(1);
+    }
+}
