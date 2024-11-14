6378aa3d93d93fe731ad9cd5e2083943039e1b2b
CaoLinh1001 <caolinh1012003@gmail.com>
2024-11-14 22:08:11 +0700

    Fix output

--- cplerings-core-application/src/main/java/com/cplerings/core/application/spouse/output/ViewSpousesOfCustomerOutput.java
+++ cplerings-core-application/src/main/java/com/cplerings/core/application/spouse/output/ViewSpousesOfCustomerOutput.java
@@ -2,10 +2,11 @@ package com.cplerings.core.application.spouse.output;
 
 import java.util.List;
 
+import com.cplerings.core.application.shared.entity.spouse.ASpouse;
 import com.cplerings.core.domain.spouse.Spouse;
 
 import lombok.Builder;
 
 @Builder
-public record ViewSpousesOfCustomerOutput(List<Spouse> spouses) {
+public record ViewSpousesOfCustomerOutput(List<ASpouse> spouses) {
 }
