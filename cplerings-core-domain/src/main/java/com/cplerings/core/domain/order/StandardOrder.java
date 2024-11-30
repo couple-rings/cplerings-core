1393eade24f135e2eb45ab0262be1975f45e6c8d
Vincent Dao <vincentdaomain@gmail.com>
2024-11-30 14:44:05 +0700

    Add more fields to entity

--- cplerings-core-domain/src/main/java/com/cplerings/core/domain/order/StandardOrder.java
+++ cplerings-core-domain/src/main/java/com/cplerings/core/domain/order/StandardOrder.java
@@ -4,15 +4,8 @@ import java.util.Set;
 
 import com.cplerings.core.common.database.DatabaseConstant;
 import com.cplerings.core.domain.account.Account;
-import com.cplerings.core.domain.shared.AbstractEntity;
+import com.cplerings.core.domain.shared.AbstractOrderEntity;
 import com.cplerings.core.domain.shared.valueobject.Money;
-
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
@@ -26,8 +19,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
@@ -36,7 +35,7 @@ import jakarta.persistence.Table;
 @AllArgsConstructor
 @Entity
 @Table(name = "tbl_standard_order")
-public class StandardOrder extends AbstractEntity {
+public class StandardOrder extends AbstractOrderEntity {
 
     private static final String STANDARD_ORDER_SEQUENCE = "standard_order_seq";
 
