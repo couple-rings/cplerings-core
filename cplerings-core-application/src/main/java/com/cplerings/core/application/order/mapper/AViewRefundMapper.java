26344d02dbab1fd423678b7925d415b41595a450
CaoLinh1001 <caolinh1012003@gmail.com>
2025-01-10 10:48:52 +0700

    Fix mapper

--- cplerings-core-application/src/main/java/com/cplerings/core/application/order/mapper/AViewRefundMapper.java
+++ cplerings-core-application/src/main/java/com/cplerings/core/application/order/mapper/AViewRefundMapper.java
@@ -3,10 +3,15 @@ package com.cplerings.core.application.order.mapper;
 import org.mapstruct.Mapper;
 
 import com.cplerings.core.application.order.output.ViewRefundOutput;
+import com.cplerings.core.application.shared.mapper.ARefundMapper;
 import com.cplerings.core.common.mapper.SpringMapperConfiguration;
 import com.cplerings.core.domain.refund.Refund;
 
-@Mapper(config = SpringMapperConfiguration.class)
+@Mapper(config = SpringMapperConfiguration.class,
+        uses = {
+            ARefundMapper.class,
+
+})
 public interface AViewRefundMapper {
 
     ViewRefundOutput toOutput(Refund refund);
